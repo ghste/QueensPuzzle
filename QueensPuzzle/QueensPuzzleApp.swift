70df8adacdbcde2e199a3e317bcf07af473ee831
Gheorghe Stegaru <gh.st@icloud.com>
2025-11-25 23:57:42 +0200

    Initial commit

---
+++ QueensPuzzle/QueensPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueensPuzzleApp.swift
+//  QueensPuzzle
+//
+//  Created by Gheorghe Stegaru on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct QueensPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QueensPuzzleTests/QueensPuzzleTests.swift
@@ -0,0 +1,17 @@
+//
+//  QueensPuzzleTests.swift
+//  QueensPuzzleTests
+//
+//  Created by Gheorghe Stegaru on 11/25/25.
+//
+
+import Testing
+@testable import QueensPuzzle
+
+struct QueensPuzzleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
